20c2361b5dc156306c395dbcc19ee5b902bae627
The-Kevin <dell@debian.dell>
2020-08-27 17:30:23 -0300

    controller para deletar postagens feito

--- src/controllers/deletePostController.ts
+++ src/controllers/deletePostController.ts
@@ -3,8 +3,20 @@ import {Request, Response} from 'express';
 
 const DeletePost = async (request: Request, response: Response) => {
 
-    
+    const { id } = request.body;
 
+        try {
+            await knex('posts')
+        .where('id', id)
+        .del();
+       
+        response.send(`post de id ${id} apagado com sucesso`)
+
+        } catch (error) {
+            console.log(error);
+            response.send('ocorreu um erro ao apagar a postagem')
+        }
+       
 }
 
 export default DeletePost;
